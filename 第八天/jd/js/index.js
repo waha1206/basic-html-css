2f2e6aa9eadd21f0d59eab43f4ed4ab6a2e5b9c6
waha1206 <waha1206@gmail.com>
2020-07-16 09:37:34 +0800

    JD練習到一個段落了

--- 第八天/jd/js/index.js
+++ 第八天/jd/js/index.js
@@ -1,13 +1,24 @@
 	window.onload = function() {
 	    var dv1Obj = document.getElementById("dv1");
 	    var i1Obj = document.getElementById("i1");
+	    var olObjs = my$("arr_ol").getElementsByTagName("li");
+
 
 
 	    console.log(dv1Obj);
 	    console.log(i1Obj);
+	    console.log(olObjs);
 
 	    i1Obj.onclick = function() {
 	        dv1Obj.style.display = "none";
 	    }
 
+	    for (var i = 0; i < olObjs.length; i++) {
+	        olObjs[i].onclick = function() {
+	            olObjs[i].removeAttribute("class");
+
+
+	            this.className = "current";
+	        };
+	    }
 	}
